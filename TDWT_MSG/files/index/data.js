b0be938a5e21e018336689ddf525ee1e487a6daa
HXRoot <907029382@qq.com>
2019-04-11 17:03:18 +0800

    add html

---
+++ TDWT_MSG/files/index/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,_(j,k),l,[m],n,_(o,p,q,r,s,t),u,_(),v,_(w,x,y,z,A,B,C,_(),D,_(E,F,G,H,I,_(J,K,L,M),N,null,O,P,Q,P,R,S,T,null,U,V,W,X,Y,Z,ba,V),bb,_(),bc,_(),bd,_(be,[])),bf,_(),bg,_());}; 
+var b="url",c="index.html",d="generationDate",e=new Date(1554799406805.71),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="sketchKeys",j="",k="s0",l="variables",m="OnLoadVariable",n="defaultBackgroundImageInfo",o="path",p="images/index/bg.jpg",q="width",r=1920,s="height",t=1200,u="viewIdToBackgroundImageInfo",v="page",w="packageId",x="cade8d820f4949c1a62c8a01946485b0",y="type",z="Axure:Page",A="name",B="index",C="notes",D="style",E="baseStyle",F="627587b6038d43cca051c114ac41ad32",G="pageAlignment",H="center",I="fill",J="fillType",K="solid",L="color",M=0xFF199ED8,N="image",O="imageHorizontalAlignment",P="near",Q="imageVerticalAlignment",R="imageRepeat",S="auto",T="favicon",U="sketchFactor",V="0",W="colorStyle",X="appliedColor",Y="fontName",Z="Applied Font",ba="borderWidth",bb="adaptiveStyles",bc="interactionMap",bd="diagram",be="objects",bf="masters",bg="objectPaths";
+return _creator();
+})());
---
+++ TDWT_MSG/files/page2/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,_(j,k),l,[m],n,_(o,p,q,r,s,t),u,_(),v,_(w,x,y,z,A,B,C,_(),D,_(E,F,G,H,I,_(J,K,L,M),N,null,O,H,P,H,Q,R,S,null,T,U,V,W,X,Y,Z,U),ba,_(),bb,_(),bc,_(bd,[])),be,_(),bf,_());}; 
+var b="url",c="page2.html",d="generationDate",e=new Date(1554799406859.42),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="sketchKeys",j="",k="s0",l="variables",m="OnLoadVariable",n="defaultBackgroundImageInfo",o="path",p="images/index/bg.jpg",q="width",r=1920,s="height",t=1200,u="viewIdToBackgroundImageInfo",v="page",w="packageId",x="c006eebc35504761b7c3d14acd82955d",y="type",z="Axure:Page",A="name",B="page2",C="notes",D="style",E="baseStyle",F="627587b6038d43cca051c114ac41ad32",G="pageAlignment",H="near",I="fill",J="fillType",K="solid",L="color",M=0xFFFFFFFF,N="image",O="imageHorizontalAlignment",P="imageVerticalAlignment",Q="imageRepeat",R="auto",S="favicon",T="sketchFactor",U="0",V="colorStyle",W="appliedColor",X="fontName",Y="Applied Font",Z="borderWidth",ba="adaptiveStyles",bb="interactionMap",bc="diagram",bd="objects",be="masters",bf="objectPaths";
+return _creator();
+})());
